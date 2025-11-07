864b61ec2b7aa1a53c74fe08dcf1a9723d828c74
SL <loehit2804@gmail.com>
2025-11-07 14:16:15 +0530

    updated project

--- FRONTEND/src/VoterApp.jsx
+++ FRONTEND/src/VoterApp.jsx
@@ -19,7 +19,7 @@ import {
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function VoterApp() {
-  const baseUrl = `http://localhost:2000`;
+  const baseUrl = `http://localhost:30098`;
   const [voter, setVoter] = useState({
     id: "",
     name: "",
